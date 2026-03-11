9c17218814e8f5bce72f02dd527c9320e32f0582
neethuzeyz <157867883+neethuzeyz@users.noreply.github.com>
2026-03-11 12:38:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,6 +1,6 @@
--- Write your PostgreSQL query statement below
 select name as customers
-from customers c
-left join  orders o
-on c.id = o.customerId
-where customerId is null;
+ from customers c
+ left join orders o
+ on c.id = o.customerId
+ where customerId is null;
+ 

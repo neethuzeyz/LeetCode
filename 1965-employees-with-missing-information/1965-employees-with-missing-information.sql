6a3bbe35f3cd6bee3aeb98683fddc940f4757e0f
Neethu Unnikrishnan <157867883+neethuzeyz@users.noreply.github.com>
2026-05-13 10:39:23 +0530

    Time: 225 ms (23.95%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below!
+select t.employee_id
+from 
+(select * from employees left join salaries using(employee_id)
+union
+select * from employees right join salaries using(employee_id))
+as t
+where t.salary is null or t.name is null
+order by employee_id;

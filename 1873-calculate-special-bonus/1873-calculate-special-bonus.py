da5d851fd8014ab83deac2e71865b67131fbd581
Neethu Unnikrishnan <157867883+neethuzeyz@users.noreply.github.com>
2026-05-29 21:55:24 +0530

    Time: 283 ms (97.2%), Space: 67.9 MB (58.44%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.py
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -4,10 +4,10 @@ def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
    import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    # Create a new column 'bonus' with default value 0
+   
     employees['bonus'] = 0
     
-    # Calculate bonus based on the conditions
+
     employees.loc[(employees['employee_id'] % 2 != 0) & (~employees['name'].str.startswith('M')), 'bonus'] = employees['salary']
     
     # Select only the required columns and sort the result table by employee_id in ascending order
